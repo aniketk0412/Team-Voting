a6cf12b3ee3b53dd3601a4922c2832f8d26c38eb
aniketk0412 <77874051+aniketk0412@users.noreply.github.com>
2023-03-22 13:41:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+ var firebaseConfig = {
+    //paste your SDK here
+  apiKey: "AIzaSyBMMKUGL-Wx8TeWfU0mgFzwROHH4x5C2pw",
+  authDomain: "pro-58-5819e.firebaseapp.com",
+  databaseURL: "https://pro-58-5819e-default-rtdb.firebaseio.com",
+  projectId: "pro-58-5819e",
+  storageBucket: "pro-58-5819e.appspot.com",
+  messagingSenderId: "987717383616",
+  appId: "1:987717383616:web:b4bdc17b51555196e24425"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
